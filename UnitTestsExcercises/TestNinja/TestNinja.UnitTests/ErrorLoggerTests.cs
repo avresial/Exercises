ad5749a4d9d55371e83f295f8752f400438c64bf
MikiKarkowski <mikikarkowski@gmail.com>
2022-11-22 22:42:25 +0100

    Added tests for Exceptions and events

---
+++ UnitTestsExcercises/TestNinja/TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+using Xunit;
+
+namespace TestNinja.UnitTests
+{
+    public class ErrorLoggerTests
+    {
+        [Fact]
+        public void Log_WhenCalled_SetTheLastErrorProperty()
+        {
+            ErrorLogger errorLogger = new ErrorLogger();
+
+            errorLogger.Log("a");
+
+            Assert.Equal("a", errorLogger.LastError);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void Log_InvalidError_ThrowArgumentNullException(string error)
+        {
+            ErrorLogger errorLogger = new ErrorLogger();
+
+            Exception ex = Record.Exception(() => errorLogger.Log(error));
+
+            Assert.IsType<ArgumentNullException>(ex);
+        }
+
+        [Fact]
+        public void Log_ValidError_RiseErrorLoggedEvent()
+        {
+            ErrorLogger errorLogger = new ErrorLogger();
+
+            var id = Guid.Empty;
+
+            errorLogger.ErrorLogged += (sender, args) => { id = args; };
+
+            errorLogger.Log("a");
+
+            Assert.NotEqual(Guid.Empty, id);
+        }
+    }
+}
