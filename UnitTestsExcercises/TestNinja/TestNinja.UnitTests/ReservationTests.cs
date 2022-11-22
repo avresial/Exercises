fddc3dcd4b97a9011a16fc675cfbb713628cdde0
MikiKarkowski <mikikarkowski@gmail.com>
2022-11-22 17:55:52 +0100

    Created first tests

--- UnitTestsExcercises/TestNinja/TestNinja.UnitTests/ReservationTests.cs
+++ UnitTestsExcercises/TestNinja/TestNinja.UnitTests/ReservationTests.cs
@@ -1,13 +1,48 @@
-﻿using Xunit;
+﻿using TestNinja.Fundamentals;
+using Xunit;
 
 namespace TestNinja.UnitTests
 {
     public class ReservationTests
     {
         [Fact]
-        public void CanBeCancelledBy() 
+        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
         {
-            Assert.True(false);
+            // Arrange
+            Reservation reservation = new Reservation();
+
+            // Act
+            bool result = reservation.CanBeCancelledBy(new User() { IsAdmin = true });
+
+            // Assert
+            Assert.True(result);
         }
+
+        [Fact]
+        public void CanBeCancelledBy_SameUserCancelingReservation_ReturnsTrue()
+        {
+            // Arrange
+            Reservation reservation = new Reservation() { MadeBy = new User() };
+
+            // Act
+            bool result = reservation.CanBeCancelledBy(reservation.MadeBy);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void CanBeCancelledBy_NotTheSameUserCancelingReservation_ReturnsTrue()
+        {
+            // Arrange
+            Reservation reservation = new Reservation() { MadeBy = new User() };
+
+            // Act
+            bool result = reservation.CanBeCancelledBy(new User());
+
+            // Assert
+            Assert.False(result);
+        }
+
     }
 }
