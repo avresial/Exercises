652d81b66af942cf505dec208579f59816a753d7
MikiKarkowski <mikikarkowski@gmail.com>
2023-01-06 22:10:38 +0100

    Refactored BookingHelper and BookingHelperTests - Lesson 75

--- UnitTestsExcercises/TestNinja/TestNinja.UnitTests/Mocking/BookingHelperTests.cs
+++ UnitTestsExcercises/TestNinja/TestNinja.UnitTests/Mocking/BookingHelperTests.cs
@@ -2,8 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TestNinja.Mocking;
 using Xunit;
 
@@ -12,16 +10,18 @@ namespace TestNinja.UnitTests.Mocking
     public class BookingHelperTests
     {
         Mock<IBookingRepository> BookingRepository;
+        Booking databaseBooking;
 
         public BookingHelperTests()
         {
             BookingRepository = new Mock<IBookingRepository>();
-            Booking booking = new Booking();
-            booking.ArrivalDate = new DateTime(2000, 1, 10);
-            booking.DepartureDate = new DateTime(2000, 1, 15);
-            booking.Reference = "DataBaseBooking";
 
-            BookingRepository.Setup(x => x.GetActiveBookings(It.IsAny<int>())).Returns(new List<Booking>() { booking }.AsQueryable());
+            databaseBooking = new Booking();
+            databaseBooking.ArrivalDate = new DateTime(2000, 1, 10);
+            databaseBooking.DepartureDate = new DateTime(2000, 1, 15);
+            databaseBooking.Reference = "DataBaseBooking";
+
+            BookingRepository.Setup(x => x.GetActiveBookings(It.IsAny<int>())).Returns(new List<Booking>() { databaseBooking }.AsQueryable());
         }
 
         [Theory]
@@ -29,12 +29,9 @@ namespace TestNinja.UnitTests.Mocking
         [InlineData(16, 17)]
         public void OverlappingBookingsExist_BookingsDoNotOverlap_ReturnsEmptyString(int arrivalDay, int departureDay)
         {
-            Booking booking = new Booking();
-            booking.ArrivalDate = new DateTime(2000, 1, arrivalDay);
-            booking.DepartureDate = new DateTime(2000, 1, departureDay);
-            booking.Reference = "1";
+            
 
-            var result = BookingHelper.OverlappingBookingsExist(booking, BookingRepository.Object);
+            var result = BookingHelper.OverlappingBookingsExist(CreateBooking(arrivalDay, departureDay), BookingRepository.Object);
 
             Assert.Equal(string.Empty, result);
         }
@@ -44,14 +41,10 @@ namespace TestNinja.UnitTests.Mocking
         [InlineData(11, 12)]
         [InlineData(10, 15)]
         [InlineData(14, 17)]
+        [InlineData(9, 17)]
         public void OverlappingBookingsExist_BookingsDoOverlap_ReturnsReferenceString(int arrivalDay, int departureDay)
         {
-            Booking booking = new Booking();
-            booking.ArrivalDate = new DateTime(2000, 1, arrivalDay);
-            booking.DepartureDate = new DateTime(2000, 1, departureDay);
-            booking.Reference = "1";
-
-            var result = BookingHelper.OverlappingBookingsExist(booking, BookingRepository.Object);
+            var result = BookingHelper.OverlappingBookingsExist(CreateBooking(arrivalDay, departureDay), BookingRepository.Object);
 
             Assert.NotEqual(string.Empty, result);
         }
@@ -59,10 +52,7 @@ namespace TestNinja.UnitTests.Mocking
         [Fact]
         public void OverlappingBookingsExist_BookingIsCanceled_ReturnsEmptyString()
         {
-            Booking booking = new Booking();
-            booking.ArrivalDate = new DateTime(2000, 1, 1);
-            booking.DepartureDate = new DateTime(2000, 1, 31);
-            booking.Reference = "1";
+            Booking booking = CreateBooking(1, 31);
             booking.Status = "Cancelled";
 
             var result = BookingHelper.OverlappingBookingsExist(booking, BookingRepository.Object);
@@ -73,23 +63,25 @@ namespace TestNinja.UnitTests.Mocking
         [Fact]
         public void OverlappingBookingsExist_AllBookingsInDatabaseAreCanceled_ReturnsReferenceString()
         {
-            BookingRepository = new Mock<IBookingRepository>();
-            Booking databaseBooking = new Booking();
-            databaseBooking.ArrivalDate = new DateTime(2000, 1, 10);
-            databaseBooking.DepartureDate = new DateTime(2000, 1, 15);
-            databaseBooking.Reference = "DataBaseBooking";
-            databaseBooking.Status = "Cancelled";
 
-            BookingRepository.Setup(x => x.GetActiveBookings(It.IsAny<int>())).Returns(new List<Booking>() { databaseBooking }.AsQueryable());
-            
-            Booking booking = new Booking();
-            booking.ArrivalDate = new DateTime(2000, 1, 1);
-            booking.DepartureDate = new DateTime(2000, 1, 31);
-            booking.Reference = "1";
+            BookingRepository.Setup(x => x.GetActiveBookings(It.IsAny<int>())).Returns(new List<Booking>() {  }.AsQueryable());
+
+            Booking booking = CreateBooking(10, 15);
 
             var result = BookingHelper.OverlappingBookingsExist(booking, BookingRepository.Object);
 
             Assert.Equal(string.Empty, result);
         }
+
+        private Booking CreateBooking(int arrivalDay, int departureDay)
+        {
+            Booking booking = new Booking();
+            booking.ArrivalDate = new DateTime(2000, 1, arrivalDay);
+            booking.DepartureDate = new DateTime(2000, 1, departureDay);
+            booking.Reference = "1";
+
+            return booking;
+        }
+
     }
 }
