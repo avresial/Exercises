240402415e16884300914f7647ff25f60eaf6eb1
MikiKarkowski <mikikarkowski@gmail.com>
2023-01-08 12:52:50 +0100

    Refactored Housekeeper - pulled out all external dependencies

---
+++ UnitTestsExcercises/TestNinja/TestNinja/Mocking/IStatementSaver.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace TestNinja.Mocking
+{
+    public interface IStatementSaver
+    {
+        string SaveStatement(int housekeeperOid, string housekeeperName, DateTime statementDate);
+    }
+}
