33ff17d3d966ef1449cf494d6a92d1a39d886e98
MikiKarkowski <mikikarkowski@gmail.com>
2023-01-08 14:32:23 +0100

    Added tests for statement being invalid - Lesson 83

--- UnitTestsExcercises/TestNinja/TestNinja.UnitTests/Mocking/HousekeeperServiceTests.cs
+++ UnitTestsExcercises/TestNinja/TestNinja.UnitTests/Mocking/HousekeeperServiceTests.cs
@@ -9,6 +9,7 @@ namespace TestNinja.UnitTests.Mocking
 {
     public class HousekeeperServiceTests
     {
+        private readonly string statementFilename = "filename";
         private Mock<IStatementSaver> statementSaver = new Mock<IStatementSaver>();
         private Mock<IEmailSender> emailService = new Mock<IEmailSender>();
         private Mock<IUnitOfWork> unitOfWork = new Mock<IUnitOfWork>();
@@ -46,6 +47,31 @@ namespace TestNinja.UnitTests.Mocking
             var result = housekeeperService.SendStatementEmails(statementDate);
 
             statementSaver.Verify(g => g.SaveStatement(housekeeper.Oid, housekeeper.FullName, statementDate), Times.Never);
+        }
+
+        [Fact]
+        public void SendStatementEmails_WhenCalled_EmailtheStatement()
+        {
+            statementSaver.Setup(g => g.SaveStatement(housekeeper.Oid, housekeeper.FullName, statementDate))
+                          .Returns(statementFilename);
+
+            var result = housekeeperService.SendStatementEmails(statementDate);
+
+            emailService.Verify(es => es.EmailFile(housekeeper.Email, housekeeper.StatementEmailBody, statementFilename, It.IsAny<string>()), Times.Once);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void SendStatementEmails_WhensatementFileIsInvalid_ShouldNotEmailTheStatement(string statement)
+        {
+            statementSaver.Setup(g => g.SaveStatement(housekeeper.Oid, housekeeper.FullName, statementDate))
+                          .Returns(statement);
+
+            var result = housekeeperService.SendStatementEmails(statementDate);
+
+            emailService.Verify(es => es.EmailFile(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()), Times.Never);
         }
     }
 }
