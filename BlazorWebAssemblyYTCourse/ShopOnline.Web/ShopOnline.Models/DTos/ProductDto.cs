99d9a47f85dd8247a879624bb18ad09127f1131f
Mikołaj Karkowski <mikikarkowski@gmail.com>
2023-08-22 15:09:17 +0200

    Created ShopOnline.Models projects

---
+++ BlazorWebAssemblyYTCourse/ShopOnline.Web/ShopOnline.Models/DTos/ProductDto.cs
@@ -0,0 +1,14 @@
+﻿namespace ShopOnline.Models.DTos
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int ImageURL { get; set; }
+        public decimal Price { get; set; }
+        public int QTY { get; set; }
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
