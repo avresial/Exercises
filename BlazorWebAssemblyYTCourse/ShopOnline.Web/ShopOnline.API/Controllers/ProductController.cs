4bbdeb22bd313f4e34ab1839dca6c4a47a870e59
Mikołaj Karkowski <mikikarkowski@gmail.com>
2023-08-22 17:02:01 +0200

    Added controller

---
+++ BlazorWebAssemblyYTCourse/ShopOnline.Web/ShopOnline.API/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopOnline.API.Repositories.Contracts;
+using ShopOnline.Models.DTos;
+
+namespace ShopOnline.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDto>>> GetItems()
+        {
+            try
+            {
+                var products = await productRepository.GetItems();
+                var productsCathegories = await productRepository.GetCategories();
+
+                if (products is null || productsCathegories is null)
+                    return NotFound();
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return null;
+        }
+    }
+}
