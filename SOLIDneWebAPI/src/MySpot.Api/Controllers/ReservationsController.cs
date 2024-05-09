6b482fcff5a17010be2b96d3be2af370e6b0827d
Mikołaj Karkowski <mikikarkowski@gmail.com>
2024-05-09 16:03:14 +0200

    Cleared project, Added ReservationsController

---
+++ SOLIDneWebAPI/src/MySpot.Api/Controllers/ReservationsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MySpot.Api.Controllers
+{
+	[Route("reservations")]
+	public class ReservationsController : ControllerBase
+	{
+		[HttpGet]
+		public void Get()
+		{
+
+		}
+
+		[HttpPost]
+		public void Post()
+		{
+
+		}
+
+	}
+}
