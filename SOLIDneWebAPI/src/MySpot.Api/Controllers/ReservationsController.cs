8ff1ecc18a18572645ebedac4bc401a529b0b5d7
Mikołaj Karkowski <mikikarkowski@gmail.com>
2024-05-09 16:49:00 +0200

    Implemented Get and Post methods in ReservationsController

--- SOLIDneWebAPI/src/MySpot.Api/Controllers/ReservationsController.cs
+++ SOLIDneWebAPI/src/MySpot.Api/Controllers/ReservationsController.cs
@@ -1,20 +1,52 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MySpot.Api.Models;
 
 namespace MySpot.Api.Controllers
 {
+	[ApiController]
 	[Route("reservations")]
 	public class ReservationsController : ControllerBase
 	{
-		[HttpGet]
-		public void Get()
+		private static readonly List<Reservation> reservations = new();
+		private readonly List<string> parkingSpotNames = new()
 		{
+			"P1", "P2", "P3", "P4", "P5"
+		};
 
+		private static int id = 1;
+
+		[HttpGet]
+		public IEnumerable<Reservation> Get() => reservations;
+
+		[HttpGet("{id:int}")]
+		public IEnumerable<Reservation> Get(int id)
+		{
+			var result = reservations.Where(x => x.Id == id);
+
+			if (result is null)
+			{
+				HttpContext.Response.StatusCode = StatusCodes.Status404NotFound;
+				return default;
+			}
+
+			return result;
 		}
 
 		[HttpPost]
-		public void Post()
+		public ActionResult Post(Reservation reservation)
 		{
+			if (parkingSpotNames.All(x => x != reservation.ParkingSpotName))
+				return BadRequest();
 
+			reservation.Date = DateTime.UtcNow.AddDays(1).Date;
+
+			if (reservations.Any(x => x.ParkingSpotName == reservation.ParkingSpotName && x.Date == reservation.Date))
+				return BadRequest();
+
+			reservation.Id = id++;
+			reservations.Add(reservation);
+
+			return CreatedAtAction(nameof(Get), new { id = reservation.Id }, null);
 		}
 
 	}
--- SOLIDneWebAPI/src/MySpot.Api/Models/Reservation.cs
+++ SOLIDneWebAPI/src/MySpot.Api/Models/Reservation.cs
@@ -2,6 +2,7 @@
 {
 	public class Reservation
 	{
+		public int Id { get; set; }
 		public string EmployeeName { get; set; }
 		public string ParkingSpotName { get; set; }
 		public string LicensePlate { get; set; }
