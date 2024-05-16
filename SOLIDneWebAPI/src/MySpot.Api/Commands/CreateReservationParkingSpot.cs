58dda386278c9fe3fb5783724f3be05a60423ba5
MikiKarkowski <mikikarkowski@gmail.com>
2024-05-16 21:22:58 +0200

    Refactor - added 2 more commands

--- SOLIDneWebAPI/src/MySpot.Api/Commands/CreateReservationParkingSpot.cs
+++ SOLIDneWebAPI/src/MySpot.Api/Commands/CreateReservationParkingSpot.cs
@@ -1,5 +1,5 @@
 ﻿namespace MySpot.Api.Commands
 {
-	public record CreateReservationParkingSpot(Guid ParkingSpotId, Guid ReservationId, string EmployeeName, string LicensePlate);
+	public record CreateReservationParkingSpot(Guid ParkingSpotId, Guid ReservationId, string EmployeeName, string LicensePlate, DateTime date);
 
 }
--- SOLIDneWebAPI/src/MySpot.Api/Dtos/ReservationDto.cs
+++ SOLIDneWebAPI/src/MySpot.Api/Dtos/ReservationDto.cs
@@ -2,8 +2,8 @@
 {
 	public class ReservationDto
 	{
-		public Guid Id { get; set; };
-		public Guid ParkingSpotId { get; set; };
+		public Guid Id { get; set; }
+		public Guid ParkingSpotId { get; set; }
 		public string EmployeeName { get; set; }
 		public string LicensePlate { get; set; }
 		public DateTime Date { get; set; }
