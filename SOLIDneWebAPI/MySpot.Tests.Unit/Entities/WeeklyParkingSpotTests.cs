0a6ef7f955977bfdde48d6bf2e22a41f1a221dfc
Mikołaj Karkowski <mikikarkowski@gmail.com>
2024-05-17 18:14:03 +0200

    Implemented initial 3 tests for weeklyParkingSpot, Added new nugget - Shouldly

--- SOLIDneWebAPI/MySpot.Tests.Unit/Entities/WeeklyParkingSpotTests.cs
+++ SOLIDneWebAPI/MySpot.Tests.Unit/Entities/WeeklyParkingSpotTests.cs
@@ -1,28 +1,68 @@
 ﻿using MySpot.Api.Entities;
 using MySpot.Api.Exceptions;
 using MySpot.Api.ValueObjects;
+using Shouldly;
 
 namespace MySpot.Tests.Unit.Entities
 {
 	public class WeeklyParkingSpotTests
 	{
+		#region Arrange
+
+		private readonly Date date;
+		private readonly WeeklyParkingSpot weeklyParkingSpot;
+
+		public WeeklyParkingSpotTests()
+		{
+			date = new Date(new DateTime(2024, 05, 17));
+			weeklyParkingSpot = new WeeklyParkingSpot(Guid.NewGuid(), new(date), "P1");
+		}
+		#endregion
 		[Fact]
-		public void given_invalid_date_add_reservation_should_fail()
+		public void given_reservation_for_not_taken_date_add_reservation_should_succeed()
 		{
 			// Arrange
-			var now = new DateTime(2024, 05, 17);
-			var invalidDate = now.AddDays(7);
+			var reservation = new Reservation(Guid.NewGuid(), weeklyParkingSpot.Id, "John Doe", "XYZ123", new Date(date.AddDays(1)));
 
-			var weeklyParkingSpot = new WeeklyParkingSpot(Guid.NewGuid(), new(now), "P1");
+			// Act
+			weeklyParkingSpot.AddReservation(reservation, new Date(date));
+
+			// Assert
+			weeklyParkingSpot.Reservations.ShouldHaveSingleItem();
+		}
+
+		[Theory]
+		[InlineData("2024-04-30")]
+		[InlineData("2024-06-01")]
+		public void given_invalid_date_add_reservation_should_fail(string dateString)
+		{
+			// Arrange
+			var invalidDate = DateTime.Parse(dateString);
+
 			var reservation = new Reservation(Guid.NewGuid(), weeklyParkingSpot.Id, "John Doe", "XYZ123", new Date(invalidDate));
 
 			// Act
-			var exception = Record.Exception(() => weeklyParkingSpot.AddReservation(reservation, new Date(now)));
+			var exception = Record.Exception(() => weeklyParkingSpot.AddReservation(reservation, new Date(date)));
 
 			// Assert
-			Assert.NotNull(exception);
-			Assert.IsType<InvalidReservationDateException>(exception);
+			exception.ShouldNotBeNull();
+			exception.ShouldBeOfType<InvalidReservationDateException>();
 		}
 
+		[Fact]
+		public void given_reservation_for_already_existing_date_add_reservation_should_fail()
+		{
+			// Arrange
+			var reservation = new Reservation(Guid.NewGuid(), weeklyParkingSpot.Id, "John Doe", "XYZ123", new Date(date.AddDays(1)));
+			weeklyParkingSpot.AddReservation(reservation, new Date(date));
+
+			var nextReservation = new Reservation(Guid.NewGuid(), weeklyParkingSpot.Id, "John Doe", "XYZ123", new Date(date.AddDays(1)));
+			// Act
+			var exception = Record.Exception(() => weeklyParkingSpot.AddReservation(nextReservation, new Date(date)));
+
+			// Assert
+			exception.ShouldNotBeNull();
+			exception.ShouldBeOfType<ParkingSpotAlreadyReservedException>();
+		}
 	}
 }
