a3478e3aef593b5ec86a09fe27d9cd8392122128
MikiKarkowski <mikikarkowski@gmail.com>
2024-06-16 14:17:12 +0200

    Refactor

---
+++ SOLIDneWebAPI/src/MySpot.Infrastructure/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MySpot.Application.Services;
+using MySpot.Core.Repositories;
+using MySpot.Infrastructure.DAL;
+using MySpot.Infrastructure.DAL.Repositories;
+using MySpot.Infrastructure.Time;
+
+namespace MySpot.Infrastructure
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddPostgress()
+                    .AddSingleton<IWeeklyParkingSpotRepository, InMemoryWeeklyParkingSpotRepository>()
+                    .AddSingleton<IClock, Clock>();
+
+
+
+            return services;
+        }
+    }
+}
