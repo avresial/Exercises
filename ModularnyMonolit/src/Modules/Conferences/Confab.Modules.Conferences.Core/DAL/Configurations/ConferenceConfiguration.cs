2a818e958344d5230c3094e3e99554cb845414b1
Mikołaj Karkowski <mikikarkowski@gmail.com>
2025-11-06 16:05:59 +0100

    Added DAL  to conferences.Core

---
+++ ModularnyMonolit/src/Modules/Conferences/Confab.Modules.Conferences.Core/DAL/Configurations/ConferenceConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using Confab.Modules.Conferences.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Confab.Modules.Conferences.Core.DAL.Configurations;
+internal class ConferenceConfiguration : IEntityTypeConfiguration<Conference>
+{
+    public void Configure(EntityTypeBuilder<Conference> builder)
+    {
+    }
+}
---
+++ ModularnyMonolit/src/Modules/Conferences/Confab.Modules.Conferences.Core/DAL/Configurations/HostConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Confab.Modules.Conferences.Core.DAL.Configurations;
+internal class HostConfiguration : IEntityTypeConfiguration<Host>
+{
+    public void Configure(EntityTypeBuilder<Host> builder)
+    {
+    }
+}
