9dd4736c1e878eacedc5388fb16dc0906c6a4d09
Mikołaj Karkowski <mikikarkowski@gmail.com>
2025-11-17 20:12:05 +0100

    Added users module

--- ModularnyMonolit/src/Modules/Speakers/Confab.Modules.Speakers.Api/Controllers/SpeakersController.cs
+++ ModularnyMonolit/src/Modules/Speakers/Confab.Modules.Speakers.Api/Controllers/SpeakersController.cs
@@ -1,50 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using Confab.Modules.Speakers.Core.DTO;
+﻿using Confab.Modules.Speakers.Core.DTO;
 using Confab.Modules.Speakers.Core.Services;
 using Confab.Shared.Abstractions.Contexts;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
-namespace Confab.Modules.Speakers.Api.Controllers
+namespace Confab.Modules.Speakers.Api.Controllers;
+
+[Authorize(Policy = _policy)]
+internal class SpeakersController(ISpeakersService service, IContext context) : BaseController
 {
-    internal class SpeakersController : BaseController
+    private const string _policy = "speakers";
+
+    [HttpGet("{id:guid}")]
+    [AllowAnonymous]
+    public async Task<ActionResult<SpeakerDto>> Get(Guid id) => OkOrNotFound(await service.GetAsync(id));
+
+    [HttpGet]
+    [AllowAnonymous]
+    public async Task<ActionResult<IEnumerable<SpeakerDto>>> Get() => Ok(await service.BrowseAsync());
+
+    [HttpPost]
+    public async Task<ActionResult> Post(SpeakerDto speaker)
     {
-        private const string Policy = "speakers";
-        private readonly ISpeakersService _speakersService;
-        private readonly IContext _context;
+        speaker.Id = context.Identity.Id;
+        await service.CreateAsync(speaker);
+        return CreatedAtAction(nameof(Get), new { id = speaker.Id }, null);
+    }
 
-        public SpeakersController(ISpeakersService service, IContext context)
-        {
-            _speakersService = service;
-            _context = context;
-        }
-
-        [HttpGet("{id:guid}")]
-        [AllowAnonymous]
-        public async Task<ActionResult<SpeakerDto>> Get(Guid id) =>  OkOrNotFound(await _speakersService.GetAsync(id));
-
-        [HttpGet]
-        [AllowAnonymous]
-        public async Task<ActionResult<IEnumerable<SpeakerDto>>> Get() => Ok(await _speakersService.BrowseAsync());
-
-        [HttpPost]
-        [Authorize]
-        public async Task<ActionResult> Post(SpeakerDto speaker)
-        {
-            speaker.Id = _context.Identity.Id;
-            await _speakersService.CreateAsync(speaker);
-            return CreatedAtAction(nameof(Get), new {id = speaker.Id}, null);
-        }
-
-        [Authorize]
-        [HttpPut("{id:guid}")]
-        public async Task<ActionResult> Put(SpeakerDto speaker)
-        {
-            speaker.Id = _context.Identity.Id;
-            await _speakersService.UpdateAsync(speaker);
-            return NoContent();
-        }
+    [Authorize]
+    [HttpPut("{id:guid}")]
+    public async Task<ActionResult> Put(SpeakerDto speaker)
+    {
+        speaker.Id = context.Identity.Id;
+        await service.UpdateAsync(speaker);
+        return NoContent();
     }
 }
