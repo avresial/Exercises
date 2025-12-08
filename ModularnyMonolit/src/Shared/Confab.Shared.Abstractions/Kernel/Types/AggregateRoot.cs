2312ae2bce5a0af4250f5770a576b519296bbcfa
Mikołaj Karkowski <mikikarkowski@gmail.com>
2025-12-08 19:21:10 +0100

    Refactor repositories to use constructor injection for DbContext and streamline method implementations

--- ModularnyMonolit/src/Shared/Confab.Shared.Abstractions/Kernel/Types/AggregateRoot.cs
+++ ModularnyMonolit/src/Shared/Confab.Shared.Abstractions/Kernel/Types/AggregateRoot.cs
@@ -1,43 +1,39 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿namespace Confab.Shared.Abstractions.Kernel.Types;
 
-namespace Confab.Shared.Abstractions.Kernel.Types
+public abstract class AggregateRoot<T>
 {
-    public abstract class AggregateRoot<T>
+    public T Id { get; protected set; }
+    public int Version { get; protected set; }
+    public IEnumerable<IDomainEvent> Events => _events;
+
+    private readonly List<IDomainEvent> _events = new();
+    private bool _versionIncremented;
+
+    protected void AddEvent(IDomainEvent @event)
     {
-        public T Id { get; protected set; }
-        public int Version { get; protected set; }
-        public IEnumerable<IDomainEvent> Events => _events;
-        
-        private readonly List<IDomainEvent> _events = new();
-        private bool _versionIncremented;
-
-        protected void AddEvent(IDomainEvent @event)
+        if (_events.Count == 0 && !_versionIncremented)
         {
-            if (!_events.Any() && !_versionIncremented)
-            {
-                Version++;
-                _versionIncremented = true;
-            }
-            
-            _events.Add(@event);
-        }
-
-        public void ClearEvents() => _events.Clear();
-
-        protected void IncrementVersion()
-        {
-            if (_versionIncremented)
-            {
-                return;
-            }
-            
             Version++;
             _versionIncremented = true;
         }
+
+        _events.Add(@event);
     }
 
-    public abstract class AggregateRoot : AggregateRoot<AggregateId>
+    public void ClearEvents() => _events.Clear();
+
+    protected void IncrementVersion()
     {
+        if (_versionIncremented)
+        {
+            return;
+        }
+
+        Version++;
+        _versionIncremented = true;
     }
+}
+
+public abstract class AggregateRoot : AggregateRoot<AggregateId>
+{
 }
