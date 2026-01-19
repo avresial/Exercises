6cb8950951347ea470cc515ec74b0fc5e1342a78
Mikołaj Karkowski <mikikarkowski@gmail.com>
2026-01-19 20:23:34 +0100

    .net update

--- ModularnyMonolit/src/Modules/Saga/Confab.Modules.Saga/SagaModule.cs
+++ ModularnyMonolit/src/Modules/Saga/Confab.Modules.Saga/SagaModule.cs
@@ -2,21 +2,20 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Confab.Modules.Saga
+namespace Confab.Modules.Saga;
+
+public class SagaModule : IModule
 {
-    public class SagaModule : IModule
+    public const string BasePath = "saga-module";
+    public string Name { get; } = "Saga";
+    public string Path => BasePath;
+
+    public void Register(IServiceCollection services)
     {
-        public const string BasePath = "saga-module";
-        public string Name { get; } = "Saga";
-        public string Path => BasePath;
+        services.AddSaga();
+    }
 
-        public void Register(IServiceCollection services)
-        {
-            services.AddSaga();
-        }
-
-        public void Use(IApplicationBuilder app)
-        {
-        }
+    public void Use(IApplicationBuilder app)
+    {
     }
 }
